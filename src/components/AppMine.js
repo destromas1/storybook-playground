18d827ea568d802796399edd437b3aa6c60a6b11
Shahjada Talukdar <shahjada@shahjadas-mbp.localdomain>
2018-06-23 14:04:44 +0200

    Test/First Compo

---
+++ src/components/AppMine.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class AppMine extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default AppMine;
