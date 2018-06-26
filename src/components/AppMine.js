d4da11a6b5b46da4949cbf10a8996e572acb84a3
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2018-06-26 19:12:50 +0200

    Added css  Modules

--- src/components/AppMine.js
+++ src/components/AppMine.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import styles from "./appMineStyles.css";
 
 class AppMine extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.redarea}>
         <header>
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
