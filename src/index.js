d920a9b6244d06e1ee7e1f69edfd8121db44f8c5
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2018-07-07 13:50:27 +0200

    Added compo to index + version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 
 export { default as AppMine } from "./components/AppMine";
 export { default as AppMineStyledCom } from "./components/AppMineStyledCom";
+export { default as AppMineStyledCss } from "./components/AppMineStyledCss";
